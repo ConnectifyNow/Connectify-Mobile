a5fdc9a53cb9b5bee095ab9955c76013c3f80746
Idan17 <idanbartov@gmail.com>
2025-01-22 21:01:55 +0200

    feat: buildOrganizationLocation

--- app/src/main/java/com/connectify/connectifyNow/repositories/Organization/FireStoreOrganizationRepository.kt
+++ app/src/main/java/com/connectify/connectifyNow/repositories/Organization/FireStoreOrganizationRepository.kt
@@ -61,7 +61,7 @@ class FireStoreOrganizationRepository {
             }
     }
 
-    fun getorganizations(since: Long, callback: (List<Organization>) -> Unit) {
+    fun getOrganizations(since: Long, callback: (List<Organization>) -> Unit) {
         apiManager.db.collection(ORGANIZATIONS_COLLECTION_PATH)
             .whereGreaterThanOrEqualTo(organization.LAST_UPDATED, Timestamp(since, 0))
             .get().addOnCompleteListener {
@@ -79,7 +79,7 @@ class FireStoreOrganizationRepository {
             }
     }
 
-    suspend fun addorganization(organization: organization, onSuccess: (String) -> Unit): String {
+    suspend fun addOrganization(organization: organization, onSuccess: (String) -> Unit): String {
         val documentReference = apiManager.db.collection(ORGANIZATIONS_COLLECTION_PATH)
             .add(organization.json)
             .await()
@@ -88,11 +88,66 @@ class FireStoreOrganizationRepository {
         return documentReference.id
     }
 
-    fun setorganizationInUserTypeDB(documentReferenceId: String) = run {
+    fun setOrganizationInUserTypeDB(documentReferenceId: String) = run {
         val userType = UserType(Type.organization)
         apiManager.db.collection(USER_TYPE_COLLECTION_PATH).document(documentReferenceId).set(userType)
     }
 
+    fun setOrganizationsOnMap(callback: (Organization) -> Unit) {
+        val organizationsReference = apiManager.db.collection(organizations_COLLECTION_PATH)
+        organizationsReference.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val organizationId = document.id
+                    fetchorganizationLocation(organizationId, callback)
+                }
+            }
+    }
 
+    private fun fetchOrganizationLocation(organizationId: String, callback: (Organization) -> Unit) {
+        val organizationDocument = apiManager.db.collection(ORGANIZATIONS_COLLECTION_PATH)
+        organizationDocument.document(organizationId).get()
+            .addOnSuccessListener { documentSnapshot ->
+                val data = documentSnapshot.data
+                if (data != null) {
+                    val locationData = data["location"] as? Map<*, *>
+                    if (locationData != null) {
+                        val organizationLocation = buildOrganizationLocation(locationData)
+
+                        val timestamp: Timestamp? = data["lastUpdated"] as? Timestamp
+                        var lastUpdated: Long? = null
+                        timestamp?.let {
+                            lastUpdated = it.seconds
+                        }
+
+                        val organization = organization(
+                            name = data["name"] as String,
+                            email = data["email"] as String,
+                            logo = data["logo"] as String,
+                            bio = data["bio"] as String,
+                            location = organizationLocation,
+                            lastUpdated = lastUpdated ?: 0 // Default to 0 if timestamp is null
+                        )
+                        callback(organization)
+                    }
+                }
+            }
+            .addOnFailureListener { locationException ->
+                Log.e("Firestore", "Error getting location documents: $locationException")
+            }
+    }
+
+    private fun buildOrganizationLocation(locationData: Map<*, *>?): organizationLocation {
+        val address = locationData?.get("address") as String
+        val geoPoint = locationData["location"] as GeoPoint
+        val latitude = geoPoint.latitude
+        val longitude = geoPoint.longitude
+
+        return organizationLocation(
+            address,
+            geoPoint,
+            GeoHash(latitude, longitude)
+        )
+    }
     
 }
