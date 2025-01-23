8b06035b33984071daebfe16be9f8836dff7c16c
Hila <hila.ohana1910@gmail.com>
2025-01-23 18:40:59 +0200

    add post Adapter

---
+++ app/src/main/java/com/connectify/connectifyNow/adapters/PostAdapter.kt
@@ -0,0 +1,121 @@
+package com.connectify.connectifyNow.adapters
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.connectify.connectifyNow.models.Post
+import com.connectify.connectifyNow.R
+import com.connectify.connectifyNow.fragments.FeedFragment
+
+class PostAdapter(var posts: MutableList<Post>, var isFromFeed: Boolean, var containArgs: Boolean = false) : RecyclerView.Adapter<PostAdapter.PostHolder>() {
+
+    var listener: FeedFragment.OnPostClickListener? = null
+    fun setOnPostClickListener(listener: FeedFragment.OnPostClickListener) {
+        this.listener = listener
+    }
+
+    class PostHolder(
+        itemView: View,
+        private val posts: List<Post>,
+        val listener: FeedFragment.OnPostClickListener?,
+        isFromFeed: Boolean
+    ) : RecyclerView.ViewHolder(itemView) {
+        val ownerNameLabel: TextView = itemView.findViewById(R.id.ownerId)
+        val contentLabel: TextView = itemView.findViewById(R.id.content)
+        val image = itemView.findViewById<ImageView>(R.id.imagePath)
+
+        val editPostButton = itemView.findViewById<ImageView>(R.id.post_edit_button)
+        val deletePostButton = itemView.findViewById<ImageView>(R.id.deletePostButton)
+        val actionsToolbar = itemView.findViewById<LinearLayout>(R.id.post_actions_toolbar);
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION && isFromFeed) {
+                    listener?.onPostClicked(posts[position])
+                    Log.d("onClickPost", posts[position].ownerId)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false)
+        return PostHolder(view, posts, listener, isFromFeed)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        val post = posts[position]
+
+
+        val currentUserID = UserUseCases().getUserId()
+        val isOwner = currentUserID == post.ownerId
+
+        if (!isFromFeed && isOwner && !containArgs) {
+            holder.actionsToolbar.visibility = View.VISIBLE;
+
+            holder.editPostButton.setOnClickListener {
+                if (holder.adapterPosition != RecyclerView.NO_POSITION) {
+                    val args = Bundle()
+                    args.putString("postId", posts[position].id)
+
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_profileFragment_to_editPost, args)
+                }
+            }
+        } else {
+            holder.actionsToolbar.visibility = View.GONE;
+        }
+
+        holder.deletePostButton.setOnClickListener {
+            deletePost(post)
+        }
+
+        if (post.imagePath.isNotBlank()) {
+            holder.image.setVisibility(View.VISIBLE)
+            val imageUri = Uri.parse(post.imagePath)
+            loadImage(imageUri, holder.image)
+        } else {
+            holder.image.visibility = View.GONE
+        }
+        holder.ownerNameLabel.text = post.title
+        holder.contentLabel.text = post.content
+    }
+
+    private fun loadImage(imageUri: Uri, imageView: ImageView) {
+        Picasso.get().load(imageUri).into(imageView)
+    }
+
+
+    fun clear() {
+        posts.clear()
+    }
+
+    fun addAll(newPosts: MutableList<Post>) {
+        newPosts.sortByDescending { it.lastUpdated };
+        posts.addAll(newPosts)
+        notifyDataSetChanged() // Notify the adapter that the data set has changed
+    }
+
+    private fun deletePost(post: Post) {
+        val position = posts.indexOf(post)
+        if (position != -1) {
+            PostViewModel().deletePost(post)
+            posts.removeAt(position) // Remove from the list
+            notifyItemRemoved(position) // Update UI
+            notifyItemRangeChanged(position, posts.size)
+        }
+    }
+}
---
+++ app/src/main/java/com/connectify/connectifyNow/fragments/FeedFragment.kt
@@ -0,0 +1,207 @@
+package com.connectify.connectifyNow.fragments
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.skillsync.ViewModel.PostViewModel
+import com.android.skillsync.ViewModel.UserAuthViewModel
+import com.android.skillsync.adapters.PostAdapter
+import com.android.skillsync.databinding.FragmentFeedBinding
+import com.android.skillsync.databinding.UserProfileDialogBinding
+import com.android.skillsync.helpers.ActionBarHelper
+import com.android.skillsync.models.Post.Post
+import com.android.skillsync.models.UserInfo
+
+class FeedFragment : Fragment() {
+    private lateinit var postsRecyclerView: RecyclerView
+    private lateinit var postAdapter: PostAdapter
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var viewModel: PostViewModel
+    private lateinit var progressBar: ProgressBar
+    private val userAuthViewModel: UserAuthViewModel by activityViewModels()
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Initialize views
+        postsRecyclerView = binding.postsRecyclerView
+        swipeRefreshLayout = binding.pullToRefresh
+        postAdapter = PostAdapter(mutableListOf(), true)
+        viewModel = ViewModelProvider(this)[PostViewModel::class.java]
+
+        postsRecyclerView.setPadding(0, 0, 0, 250)
+
+        postAdapter.posts = viewModel.posts.value?.toMutableList() ?: mutableListOf()
+        progressBar = binding.progressBar
+        progressBar.visibility = View.VISIBLE
+
+
+        userAuthViewModel.getInfoOnUser(userAuthViewModel.getUserId().toString()) { userInfo, error ->
+            when (userInfo) {
+                is UserInfo.UserStudent -> {
+                    (activity as MainActivity).setProfile("USER")
+                }
+                is UserInfo.UserCompany -> {
+                    (activity as MainActivity).setProfile("COMPANY")
+                }
+
+                null -> Log.d("InfoUser", "we have identify unknown user")
+            }
+        }
+
+        // Set up RecyclerView
+        postsRecyclerView.layoutManager = LinearLayoutManager(context)
+        postsRecyclerView.adapter = postAdapter
+
+        swipeRefreshLayout.setOnRefreshListener {
+            reloadData()
+        }
+
+        // Set up ViewModel
+        viewModel.posts.observe(viewLifecycleOwner) { newPosts ->
+            // Clear the existing data in the adapter
+            if (postAdapter.posts.isNotEmpty()) {
+                postAdapter.clear()
+            }
+            postAdapter.addAll(newPosts)
+            postAdapter.notifyDataSetChanged()
+            progressBar.visibility = View.GONE
+        }
+
+        postAdapter.setOnPostClickListener(object : OnPostClickListener {
+            override fun onPostClicked(post: Post?) {
+                post?.let {
+                    userAuthViewModel.getInfoOnUser(it.ownerId) { userInfo, error ->
+                        if (error != null) {
+                            Log.e("User Info Error", error)
+                        } else {
+                            val dialogBinding = UserProfileDialogBinding.inflate(layoutInflater)
+                            val dialog = AlertDialog.Builder(requireContext())
+                                .setView(dialogBinding.root)
+                                .create()
+
+                            when (userInfo) {
+                                is UserInfo.UserStudent -> {
+                                    dialogBinding.textViewName.text = userInfo.studentInfo?.name
+                                    dialogBinding.textViewEmail.text = userInfo.studentInfo?.email
+                                    dialogBinding.textViewBio.text = userInfo.studentInfo?.bio
+                                }
+
+                                is UserInfo.UserCompany -> {
+                                    dialogBinding.textViewName.text = userInfo.companyInfo?.name
+                                    dialogBinding.textViewEmail.text = userInfo.companyInfo?.email
+                                    dialogBinding.textViewBio.text = userInfo.companyInfo?.bio
+                                }
+
+                                null -> throw IllegalArgumentException("unknown userInfo")
+                            }
+
+                            val fragmentManager = requireActivity().supportFragmentManager
+                            val fragmentTransaction = fragmentManager.beginTransaction()
+                            val profileFragment = ProfileFragment()
+
+                            dialogBinding.buttonMoreDetails.setOnClickListener {
+                                dialog.dismiss()
+                                when (userInfo) {
+                                    is UserInfo.UserStudent -> {
+                                        userInfo.studentInfo?.id?.let { userId ->
+
+                                            val args = Bundle()
+                                            args.putString("userId", userId)
+                                            profileFragment.arguments = args
+
+                                            fragmentTransaction.replace(
+                                                R.id.container,
+                                                profileFragment
+                                            )
+                                            fragmentTransaction.addToBackStack("profileBackStack")
+
+                                            fragmentTransaction.commit()
+                                        }
+                                    }
+
+                                    is UserInfo.UserCompany -> {
+                                        userInfo.companyInfo?.id?.let { companyId ->
+
+                                            val args = Bundle()
+                                            args.putString("userId", companyId)
+                                            profileFragment.arguments = args
+
+                                            fragmentTransaction.replace(
+                                                R.id.container,
+                                                profileFragment
+                                            )
+                                            fragmentTransaction.addToBackStack("profileBackStack")
+
+                                            fragmentTransaction.commit()
+                                            dialog.dismiss()
+                                        }
+                                    }
+                                }
+                            }
+                            dialog.show()
+                        }
+                    }
+                }
+            }
+
+            override fun onPostClick(position: Int) {
+                Log.d("TAG", "postsRecyclerAdapter: post click position $position")
+            }
+        })
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ActionBarHelper.showActionBarAndBottomNavigationView(requireActivity() as? AppCompatActivity)
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun reloadData() {
+        swipeRefreshLayout.isRefreshing = false
+        progressBar.visibility = View.VISIBLE
+        viewModel.refreshPosts()
+        progressBar.visibility = View.GONE
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onResume() {
+        super.onResume()
+        reloadData()
+    }
+
+    interface OnPostClickListener {
+        fun onPostClick(position: Int)
+        fun onPostClicked(post: Post?)
+    }
+}
