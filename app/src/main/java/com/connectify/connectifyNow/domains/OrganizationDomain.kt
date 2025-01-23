b12ffe0e346bb824342585cfdd8d05677603059a
Hila <hila.ohana1910@gmail.com>
2025-01-23 19:59:02 +0200

    pull from dev

---
+++ app/src/main/java/com/connectify/connectifyNow/domains/OrganizationDomain.kt
@@ -0,0 +1,56 @@
+package com.connectify.connectifyNow.domains
+import android.util.Log
+import com.connectify.connectifyNow.models.Organization
+import com.connectify.connectifyNow.repositories.Auth.FireStoreAuthRepository
+import com.connectify.connectifyNow.repositories.Organization.FireStoreOrganizationRepository
+import com.connectify.connectifyNow.repositories.Organization.LocalStoreOrganizationRepository
+import java.util.concurrent.Executors
+
+class OrganizationDomain {
+    private val localStoreOrganizationRepository: LocalStoreOrganizationRepository = LocalStoreOrganizationRepository()
+    private val fireStoreOrganizationRepository: FireStoreOrganizationRepository = FireStoreOrganizationRepository()
+    val fireStoreAuthRepository: FireStoreAuthRepository = FireStoreAuthRepository()
+
+    private var executor = Executors.newSingleThreadExecutor()
+
+    fun getOrganizationById(id: String, callback: (organization: Organization) -> Unit) {
+        fireStoreOrganizationRepository.getOrganization(id, callback);
+    }
+
+    fun refreshOragnizations() {
+
+        val lastUpdated: Long = Organization.lastUpdated
+
+        fireStoreOrganizationRepository.getOrganizations(lastUpdated) { organizations ->
+            Log.i("TAG", "Firebase returned ${organizations.size}, lastUpdated: $lastUpdated")
+            executor.execute {
+                var time = lastUpdated
+                for (organization in organizations) {
+                    localStoreOrganizationRepository.add(organization)
+
+                    organization.lastUpdated?.let {
+                        if (time < it)
+                            time = organization.lastUpdated ?: System.currentTimeMillis()
+                    }
+                }
+
+                Organization.lastUpdated = time
+            }
+        }
+    }
+
+    suspend fun addOrganization(organization: Organization) {
+        fireStoreOrganizationRepository.addOrganization(organization) { id ->
+            fireStoreOrganizationRepository.setOrganizationUserType(id)
+            refreshOragnizations()
+        }
+    }
+
+    suspend fun updateOrganization(organization: Organization, data: Map<String, Any>, onSuccessCallBack: () -> Unit, onFailureCallBack: () -> Unit) {
+        fireStoreOrganizationRepository.updateOrganization(organization, data, onSuccessCallBack, onFailureCallBack)
+    }
+
+    fun setOrganizationsOnMap(callback: (Organization) -> Unit) {
+        fireStoreOrganizationRepository.setOrganizationsOnMap(callback)
+    }
+}
