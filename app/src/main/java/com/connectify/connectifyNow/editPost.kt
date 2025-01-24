5318e74f6e74327de5371daa1c28b41f34b3ec83
Idan17 <idanbartov@gmail.com>
2025-01-24 11:20:06 +0200

    feat: dynamic text helper

--- app/src/main/java/com/connectify/connectifyNow/editPost.kt
+++ app/src/main/java/com/connectify/connectifyNow/editPost.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.connectify.connectifyNow.databinding.FragmentEditPostBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [editPost.newInstance] factory method to
- * create an instance of this fragment.
- */
 class editPost : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentEditPostBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var view: View
+    private lateinit var dynamicTextHelper: DynamicTextHelper
+    private lateinit var postViewModel: PostViewModel
+    private lateinit var imageHelper: ImageHelper
+    private lateinit var imageView: ImageView
+    private  lateinit var loadingOverlay: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
