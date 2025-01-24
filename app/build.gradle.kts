118dfbf10e3abf7b9f0930390aa264350b782e03
Idan17 <idanbartov@gmail.com>
2025-01-24 12:17:26 +0200

    Feat: layout styles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,5 +76,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation("com.airbnb.android:lottie:3.6.0")
+
 
 }
